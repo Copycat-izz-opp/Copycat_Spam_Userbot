8fb5fee7a49c0183997d672cdfea2c129b765170
Copycat-izz-opp <85601169+Copycat-izz-opp@users.noreply.github.com>
2021-08-10 22:42:43 +0530

    stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -19,7 +19,7 @@ while True:
             session = client.session.save()
             client.send_message(
                 "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @Copycat_Spam For Any Help!\n\n",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @Copycat_Userbot For Any Help!\n\n",
             )
 
             print(
